947218f5e29dcea360e77590d8aa7ad2a3153f90
Giulio <giulio.rebuffo@gmail.com>
2018-08-24 12:45:15 +0200

    indentation improvements

--- chaincode/Messanger/Messanger.go
+++ chaincode/Messanger/Messanger.go
@@ -7,60 +7,59 @@ import (
     "encoding/json"
 )
 
-type Messanger struct {
-}
+type Messanger struct {}
 
 // User Definition
 type User struct {
-    Msgs [] string
+    Msgs[] string
 }
 
-func (c *Messanger) Init(stub shim.ChaincodeStubInterface) pb.Response {
+func(c * Messanger) Init(stub shim.ChaincodeStubInterface) pb.Response {
     return shim.Success(nil)
 }
 
-func (c *Messanger) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+func(c * Messanger) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 
-    function, args := stub.GetFunctionAndParameters()
+    function, args: = stub.GetFunctionAndParameters()
 
     if function == "sendMessage" {
         if len(args) != 4 {
             return shim.Error("Invalid Arguments Number")
         }
         // init args
-        loginArgs := make([][]byte, 3)
+        loginArgs: = make([][] byte, 3)
         // login object
-        loginArgs[0]= []byte("login")
-        loginArgs[1]= []byte(args[0])
-        loginArgs[2]= []byte(args[1])
+        loginArgs[0] = [] byte("login")
+        loginArgs[1] = [] byte(args[0])
+        loginArgs[2] = [] byte(args[1])
         // check response
-        loginRes := stub.InvokeChaincode("Authenticator",  loginArgs, "")
+        loginRes: = stub.InvokeChaincode("Authenticator", loginArgs, "")
         if loginRes.Status != 200 {
             return shim.Error("Error in login")
         }
         // verify if the recipient exist
-        existArgs := make([][]byte, 2)
+        existArgs: = make([][] byte, 2)
         // login object
-        existArgs[0]= []byte("exist")
-        existArgs[1]= []byte(args[0])
+        existArgs[0] = [] byte("exist")
+        existArgs[1] = [] byte(args[0])
         // check response
-        existRes := stub.InvokeChaincode("Authenticator",  existArgs, "")
+        existRes: = stub.InvokeChaincode("Authenticator", existArgs, "")
         if existRes.Status != 200 {
             return shim.Error("User not found")
         }
         // get user
-        v, err := stub.GetState(args[2])
+        v, err: = stub.GetState(args[2])
         if err != nil {
             return shim.Error(err.Error())
         }
         // send message
         var user User;
-        json.Unmarshal(v, &user)
+        json.Unmarshal(v, & user)
         if len(user.Msgs) == 0 {
-            user.Msgs = make([]string, 0)
+            user.Msgs = make([] string, 0)
         }
         user.Msgs = append(user.Msgs, args[0] + ": " + args[3])
-        userAsBytes, err := json.Marshal(user)
+        userAsBytes, err: = json.Marshal(user)
         if err != nil {
             return shim.Error(err.Error())
         }
@@ -78,31 +77,31 @@ func (c *Messanger) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
             return shim.Error("Invalid Arguments Number")
         }
         // init args
-        loginArgs := make([][]byte, 3)
+        loginArgs: = make([][] byte, 3)
         // login object
-        loginArgs[0]= []byte("login")
-        loginArgs[1]= []byte(args[0])
-        loginArgs[2]= []byte(args[1])
+        loginArgs[0] = [] byte("login")
+        loginArgs[1] = [] byte(args[0])
+        loginArgs[2] = [] byte(args[1])
         // check response
-        loginRes := stub.InvokeChaincode("Authenticator",  loginArgs, "")
+        loginRes: = stub.InvokeChaincode("Authenticator", loginArgs, "")
         if loginRes.Status != 200 {
             return shim.Error("Error in login")
         }
         // get user
-        v, err := stub.GetState(args[0])
+        v, err: = stub.GetState(args[0])
         if err != nil {
             return shim.Error(err.Error())
         }
         // send message
         var user User;
-        json.Unmarshal(v, &user)
-        i := 0
-        retval := ""
+        json.Unmarshal(v, & user)
+        i: = 0
+        retval: = ""
         for i < len(user.Msgs) {
             retval = retval + user.Msgs[i] + ", "
             i = i + 1;
         }
-        return shim.Success([]byte(retval))
+        return shim.Success([] byte(retval))
     } else {
         return shim.Error("Invalid Command")
     }
@@ -111,7 +110,7 @@ func (c *Messanger) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 }
 
 func main() {
-    err := shim.Start(new(Messanger))
+    err: = shim.Start(new(Messanger))
     if err != nil {
         fmt.Printf("Error starting chaincode sample: %s", err)
     }
